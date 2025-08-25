f22b59ce53d433139100a96520971be552cd9a31
divyanand-spec <divyanandchauhan0707@gmail.com>
2025-08-26 00:57:47 +0530

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,46 @@
+const menuOpenButton = document.querySelector('#menu-open-button');
+const menuCloseButton = document.querySelector('#menu-close-button');
+const navLinks = document.querySelectorAll('.nav-menu .nav-link');
+
+menuOpenButton.addEventListener("click", ()=>{
+    document.body.classList.toggle("show-mobile-menu")
+})
+
+menuCloseButton.addEventListener("click",()=> menuOpenButton.click() )
+ 
+menuCloseButton.forEach(lik => {
+    link.addEventListener("click",() => menuOpenButton.click())
+})
+
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween: 25,
+  grabursor:true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dyamicBullets: true, 
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
